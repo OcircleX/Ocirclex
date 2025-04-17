6fef6c6f7a6fce3afd3bb4802313e09744316ee8
OcircleX <circlex02@gmail.com>
2025-04-17 14:46:48 +0500

    chgh

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -174,7 +174,7 @@ const Services = () => {
 
 <div className='w-full  flex max-w-screen-xl   h-[300px] justify-between mt-12'>
     <div className='md:w-[28%]  relative hidden sm:block' id="imgthree">
-        <Image src="/cubea.jpg" fill alt='container' />
+        <Image src="/Container.png" fill alt='container' />
 
     </div>
 
