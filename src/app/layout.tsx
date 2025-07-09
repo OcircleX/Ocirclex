a89eb4daace4e9846eec141e99f22b8e551d991b
OcircleX <circlex02@gmail.com>
2025-07-09 18:25:27 +0500

    tawk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,6 +43,23 @@ export default function RootLayout({
       <body>
            {/* <Navbar /> */}
            <main className="h-[80%] flex justify-center">{children}</main>
+             {/* ✅ Tawk.to Live Chat Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+              var s1=document.createElement("script"),
+              s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/686cc6b01731c019090354cc/1ivkeg916';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
           <Footer />
 
       </body>
